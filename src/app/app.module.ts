24f7341aef346abe1fc4ba957e7adf5b2029abd0
michidoma <michidmaap6@gmail.com>
2023-01-11 09:10:04 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { ContentSectionComponent } from './content-section/content-section.component';
+import { FilterComponent } from './filter/filter.component';
+import { VideoCardComponent } from './video-card/video-card.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SideBarComponent,
+    ContentSectionComponent,
+    FilterComponent,
+    VideoCardComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
